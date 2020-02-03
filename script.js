acaadb963cd124d5ed7f84f3eee3d6d559ca5470
Samin Yeaser <saminyeaser1@gmail.com>
2020-02-04 01:04:28 +0600

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+function saminGen() {
+    var div = document.getElementById('box');
+    var me = document.createElement('img');
+    me.src = "https://media-exp1.licdn.com/dms/image/C5103AQHGmyP0w6_bMQ/profile-displayphoto-shrink_200_200/0?e=1586390400&v=beta&t=I7sa23UGKYWF8FVC9EFXKV04FtE7ZhurJ5ioIFkIp8Q";
+    div.appendChild(me);
+}
